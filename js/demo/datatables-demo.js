08ca45c2441c56ea2f6507c370e2f8dcb7e764fe
Ícaro Forte <icarofort@gmail.com>
2019-01-12 13:08:25 -0300

    Add pagination on Table

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,10 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#myTable').DataTable();
+$.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback', function(data) {
+    
 });
+
+$(document).ready(function() {
+    $('#dataTable').DataTable( {
+        "ajax": 'xmlhttp'
+    } );
+} );
